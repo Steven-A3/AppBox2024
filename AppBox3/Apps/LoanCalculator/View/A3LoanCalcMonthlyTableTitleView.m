5260a092b53eafcc62aee4b398cd991d8d49032a
kimjeonghwan <dotnetguy83@gmail.com>
2014-03-18 18:56:52 +0900

    *LoanCalc - monthly title & cell 각 뷰의 Frame 비율로 나눔.

--- AppBox3/Apps/LoanCalculator/View/A3LoanCalcMonthlyTableTitleView.m
+++ AppBox3/Apps/LoanCalculator/View/A3LoanCalcMonthlyTableTitleView.m
@@ -87,31 +87,48 @@
 
 - (void)layoutSubviews
 {
+    // Date 16%  Payment 21%  Principal 21%  Interest 19% Balance 23%
+    
     [super layoutSubviews];
     
-    if (IS_IPHONE) {
-        dateLB.frame = CGRectMake(15, 0, 51, 35);
-        paymentLB.frame = CGRectMake(15+51, 0, 63, 35);
-        principalLB.frame = CGRectMake(15+51+63, 0, 63, 35);
-        interestLB.frame = CGRectMake(15+51+63+63, 0, 58, 35);
-        balanceLB.frame = CGRectMake(15+51+63+63+58, 0, 70, 35);
-    }
-    else {
-        if (IS_LANDSCAPE) {
-            dateLB.frame = CGRectMake(28, 0, 100, 35);
-            paymentLB.frame = CGRectMake(28+100+10, 0, 140-10-10, 35);
-            principalLB.frame = CGRectMake(28+100+140+10, 0, 140-10-10, 35);
-            interestLB.frame = CGRectMake(28+100+140+140+10, 0, 140-10-10, 35);
-            balanceLB.frame = CGRectMake(28+100+140+140+140+10, 0, 156-10-10, 35);
-        }
-        else {
-            dateLB.frame = CGRectMake(28, 0, 110, 35);
-            paymentLB.frame = CGRectMake(28+110+10, 0, 150-10-10, 35);
-            principalLB.frame = CGRectMake(28+110+150+10, 0, 150-10-10, 35);
-            interestLB.frame = CGRectMake(28+110+150+150+10, 0, 150-10-10, 35);
-            balanceLB.frame = CGRectMake(28+110+150+150+150+10, 0, 180-10-10, 35);
-        }
-    }
+//    if (IS_IPHONE) {
+//        
+//        dateLB.frame = CGRectMake(15, 0, 51, 35);
+//        paymentLB.frame = CGRectMake(15+51, 0, 63, 35);
+//        principalLB.frame = CGRectMake(15+51+63, 0, 63, 35);
+//        interestLB.frame = CGRectMake(15+51+63+63, 0, 58, 35);
+//        balanceLB.frame = CGRectMake(15+51+63+63+58, 0, 70, 35);
+//    }
+//    else {
+//        if (IS_LANDSCAPE) {
+//            dateLB.frame = CGRectMake(28, 0, 100, 35);
+//            paymentLB.frame = CGRectMake(28+100+10, 0, 140-10-10, 35);
+//            principalLB.frame = CGRectMake(28+100+140+10, 0, 140-10-10, 35);
+//            interestLB.frame = CGRectMake(28+100+140+140+10, 0, 140-10-10, 35);
+//            balanceLB.frame = CGRectMake(28+100+140+140+140+10, 0, 156-10-10, 35);
+//        }
+//        else {
+//            dateLB.frame = CGRectMake(28, 0, 110, 35);
+//            paymentLB.frame = CGRectMake(28+110+10, 0, 150-10-10, 35);
+//            principalLB.frame = CGRectMake(28+110+150+10, 0, 150-10-10, 35);
+//            interestLB.frame = CGRectMake(28+110+150+150+10, 0, 150-10-10, 35);
+//            balanceLB.frame = CGRectMake(28+110+150+150+150+10, 0, 180-10-10, 35);
+//        }
+//    }
+    
+
+    CGFloat leftInset = IS_IPHONE ? 15 : 28;
+    CGFloat dateWidth = ceilf((CGRectGetWidth(self.bounds) - leftInset) * 0.16);
+    CGFloat paymentWidth = ceilf((CGRectGetWidth(self.bounds) - leftInset) * 0.21);
+    CGFloat principalWdith = ceilf((CGRectGetWidth(self.bounds) - leftInset) * 0.21);
+    CGFloat interestWidth = ceilf((CGRectGetWidth(self.bounds) - leftInset) * 0.19);
+    CGFloat balanceWidth = ceilf((CGRectGetWidth(self.bounds) - leftInset) * 0.23);
+    
+    dateLB.frame = CGRectMake(leftInset, 0, dateWidth, 35);
+    paymentLB.frame = CGRectMake(leftInset + dateWidth, 0, paymentWidth, 35);
+    principalLB.frame = CGRectMake(leftInset + dateWidth + paymentWidth, 0, principalWdith, 35);
+    interestLB.frame = CGRectMake(leftInset + dateWidth + paymentWidth + principalWdith, 0, interestWidth, 35);
+    balanceLB.frame = CGRectMake(leftInset + dateWidth + paymentWidth + principalWdith + interestWidth, 0, balanceWidth, 35);
 }
 
 @end
--- AppBox3/Apps/LoanCalculator/View/A3LoanCalcPaymentInfoCell.m
+++ AppBox3/Apps/LoanCalculator/View/A3LoanCalcPaymentInfoCell.m
@@ -57,11 +57,12 @@
     _interestLb.backgroundColor = [UIColor clearColor];
     _balanceLb.backgroundColor = [UIColor clearColor];
     
-    _dateLb.textAlignment = NSTextAlignmentLeft;
-    _paymentLb.textAlignment = NSTextAlignmentRight;
-    _principalLb.textAlignment = NSTextAlignmentRight;
-    _interestLb.textAlignment = NSTextAlignmentRight;
-    _balanceLb.textAlignment = NSTextAlignmentRight;
+//    _dateLb.textAlignment = NSTextAlignmentLeft;
+//    _paymentLb.textAlignment = NSTextAlignmentRight;
+//    _principalLb.textAlignment = NSTextAlignmentRight;
+//    _interestLb.textAlignment = NSTextAlignmentRight;
+//    _balanceLb.textAlignment = NSTextAlignmentRight;
+    
 }
 
 
@@ -69,31 +70,47 @@
 {
     [super layoutSubviews];
     
+//    float cellHeight = 42.0;
+//
+//    if (IS_IPHONE) {
+//        _dateLb.frame = CGRectMake(15, 1, 51, cellHeight);
+//        _paymentLb.frame = CGRectMake(15+51, 1, 63-5, cellHeight);
+//        _principalLb.frame = CGRectMake(15+51+63, 1, 63-5, cellHeight);
+//        _interestLb.frame = CGRectMake(15+51+63+63, 1, 58-5, cellHeight);
+//        _balanceLb.frame = CGRectMake(15+51+63+63+58, 1, 70-5, cellHeight);
+//    }
+//    else {
+//        if (IS_LANDSCAPE) {
+//            _dateLb.frame = CGRectMake(28, 1, 100, cellHeight);
+//            _paymentLb.frame = CGRectMake(28+100+10, 1, 140-10-10, cellHeight);
+//            _principalLb.frame = CGRectMake(28+100+140+10, 1, 140-10-10, cellHeight);
+//            _interestLb.frame = CGRectMake(28+100+140+140+10, 1, 140-10-10, cellHeight);
+//            _balanceLb.frame = CGRectMake(28+100+140+140+140+10, 1, 156-10-10, cellHeight);
+//        }
+//        else {
+//            _dateLb.frame = CGRectMake(28, 1, 110, cellHeight);
+//            _paymentLb.frame = CGRectMake(28+110+10, 1, 150-10-10, cellHeight);
+//            _principalLb.frame = CGRectMake(28+110+150+10, 1, 150-10-10, cellHeight);
+//            _interestLb.frame = CGRectMake(28+110+150+150+10, 1, 150-10-10, cellHeight);
+//            _balanceLb.frame = CGRectMake(28+110+150+150+150+10, 1, 180-10-10, cellHeight);
+//        }
+//    }
+    
+    // Date 16%  Payment 21%  Principal 21%  Interest 19% Balance 23%
     float cellHeight = 42.0;
-
-    if (IS_IPHONE) {
-        _dateLb.frame = CGRectMake(15, 1, 51, cellHeight);
-        _paymentLb.frame = CGRectMake(15+51, 1, 63-5, cellHeight);
-        _principalLb.frame = CGRectMake(15+51+63, 1, 63-5, cellHeight);
-        _interestLb.frame = CGRectMake(15+51+63+63, 1, 58-5, cellHeight);
-        _balanceLb.frame = CGRectMake(15+51+63+63+58, 1, 70-5, cellHeight);
-    }
-    else {
-        if (IS_LANDSCAPE) {
-            _dateLb.frame = CGRectMake(28, 1, 100, cellHeight);
-            _paymentLb.frame = CGRectMake(28+100+10, 1, 140-10-10, cellHeight);
-            _principalLb.frame = CGRectMake(28+100+140+10, 1, 140-10-10, cellHeight);
-            _interestLb.frame = CGRectMake(28+100+140+140+10, 1, 140-10-10, cellHeight);
-            _balanceLb.frame = CGRectMake(28+100+140+140+140+10, 1, 156-10-10, cellHeight);
-        }
-        else {
-            _dateLb.frame = CGRectMake(28, 1, 110, cellHeight);
-            _paymentLb.frame = CGRectMake(28+110+10, 1, 150-10-10, cellHeight);
-            _principalLb.frame = CGRectMake(28+110+150+10, 1, 150-10-10, cellHeight);
-            _interestLb.frame = CGRectMake(28+110+150+150+10, 1, 150-10-10, cellHeight);
-            _balanceLb.frame = CGRectMake(28+110+150+150+150+10, 1, 180-10-10, cellHeight);
-        }
-    }
+    
+    CGFloat leftInset = IS_IPHONE ? 15 : 28;
+    CGFloat dateWidth = ceilf((CGRectGetWidth(self.bounds) - leftInset) * 0.16);
+    CGFloat paymentWidth = ceilf((CGRectGetWidth(self.bounds) - leftInset) * 0.21);
+    CGFloat principalWdith = ceilf((CGRectGetWidth(self.bounds) - leftInset) * 0.21);
+    CGFloat interestWidth = ceilf((CGRectGetWidth(self.bounds) - leftInset) * 0.19);
+    CGFloat balanceWidth = ceilf((CGRectGetWidth(self.bounds) - leftInset) * 0.23);
+    
+    _dateLb.frame = CGRectMake(leftInset, 1, dateWidth, cellHeight);
+    _paymentLb.frame = CGRectMake(leftInset + dateWidth, 1, paymentWidth, cellHeight);
+    _principalLb.frame = CGRectMake(leftInset + dateWidth + paymentWidth, 1, principalWdith, cellHeight);
+    _interestLb.frame = CGRectMake(leftInset + dateWidth + paymentWidth + principalWdith, 1, interestWidth, cellHeight);
+    _balanceLb.frame = CGRectMake(leftInset + dateWidth + paymentWidth + principalWdith + interestWidth, 1, balanceWidth, cellHeight);
 }
 
 @end
