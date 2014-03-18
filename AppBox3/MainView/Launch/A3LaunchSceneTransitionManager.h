56e3d1f5f5b87ce8da449c9d1bdd0218e7afb456
AllAboutApps <cs@allaboutapps.net>
2014-03-18 17:34:57 +0900

    Add  "What's New" scenes

---
+++ AppBox3/MainView/Launch/A3LaunchSceneTransitionManager.h
@@ -0,0 +1,13 @@
+//
+//  A3LaunchSceneTransitionManager.h
+//  AppBox3
+//
+//  Created by A3 on 3/17/14.
+//  Copyright (c) 2014 ALLABOUTAPPS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface A3LaunchSceneTransitionManager : NSObject <UIViewControllerAnimatedTransitioning>
+
+@end
---
+++ AppBox3/MainView/Launch/A3LaunchViewController.h
@@ -0,0 +1,19 @@
+//
+//  A3LaunchViewController.h
+//  AppBox3
+//
+//  Created by A3 on 3/17/14.
+//  Copyright (c) 2014 ALLABOUTAPPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol A3LaunchSceneViewControllerDelegate <NSObject>
+- (void)useICloudButtonPressedInViewController:(UIViewController *)viewController;
+- (void)continueButtonPressedInViewController:(UIViewController *)viewController;
+- (void)useAppBoxButtonPressedInViewController:(UIViewController *)viewController;
+@end
+
+@interface A3LaunchViewController : UIViewController <A3LaunchSceneViewControllerDelegate>
+
+@end
