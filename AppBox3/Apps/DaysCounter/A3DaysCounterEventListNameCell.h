800964b6091f2cbd7381cb3b2185171ce7b46d0e
dotnetguy83 <dotnetguy83@gmail.com>
2014-03-24 23:23:40 +0900

    *DaysCounter - EventList, Cell Layout 수정중.

---
+++ AppBox3/Apps/DaysCounter/A3DaysCounterEventListNameCell.h
@@ -0,0 +1,16 @@
+//
+//  A3DaysCounterEventListNameCell.h
+//  AppBox3
+//
+//  Created by dotnetguy83 on 3/24/14.
+//  Copyright (c) 2014 ALLABOUTAPPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface A3DaysCounterEventListNameCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *sinceLeadingConst;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *nameLeadingConst;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *photoLeadingConst;
+@end
---
+++ AppBox3/Apps/DaysCounter/A3DaysCounterEventListNameCell.m
@@ -0,0 +1,34 @@
+//
+//  A3DaysCounterEventListNameCell.m
+//  AppBox3
+//
+//  Created by dotnetguy83 on 3/24/14.
+//  Copyright (c) 2014 ALLABOUTAPPS. All rights reserved.
+//
+
+#import "A3DaysCounterEventListNameCell.h"
+
+@implementation A3DaysCounterEventListNameCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
