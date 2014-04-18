2673d0dd8235e019bc1732a5257319bbcc4eefe3
Soon Gyu Kim <etthori@gmail.com>
2014-04-17 22:19:10 -0400

    아이패드 계산기 하단에 라인 삭제.

--- AppBox3/Apps/Calculator/A3CalculatorViewController_iPad.m
+++ AppBox3/Apps/Calculator/A3CalculatorViewController_iPad.m
@@ -83,7 +83,7 @@
 
 	_textFieldForPlayInputClick = [[UITextField alloc] initWithFrame:CGRectZero];
 	_textFieldForPlayInputClick.delegate = self;
-	_inputViewForPlayInputClick = [[A3KeyboardView alloc] initWithFrame:CGRectMake(0, 0, 1, 1)];
+	_inputViewForPlayInputClick = [[A3KeyboardView alloc] initWithFrame:CGRectMake(0, 0, 1, 0.1)];
 	_textFieldForPlayInputClick.inputView = _inputViewForPlayInputClick;
 	[self.view addSubview:_textFieldForPlayInputClick];
 
