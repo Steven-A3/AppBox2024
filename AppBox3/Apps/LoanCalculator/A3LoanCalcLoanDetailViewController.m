783357627aa63e5d887b3f7a1f54d6fdc4043e6d
dotnetguy83 <dotnetguy83@gmail.com>
2014-07-03 17:46:20 +0900

    *LoanCalc - Compare, 기간 표현 버그 수정.

--- AppBox3/Apps/LoanCalculator/A3LoanCalcLoanDetailViewController.m
+++ AppBox3/Apps/LoanCalculator/A3LoanCalcLoanDetailViewController.m
@@ -225,9 +225,7 @@ NSString *const A3LoanCalcLoanGraphCellID2 = @"A3LoanCalcLoanGraphCell";
         }
         case A3LC_CalculationItemTerm:
         {
-            placeHolderText = NSLocalizedString(@"0 year", nil);
-            int yearInt =  (int)round(self.loanData.monthOfTerms.doubleValue/12.0);
-            textFieldText = [NSString stringWithFormat:NSLocalizedStringFromTable(@"%ld years", @"StringsDict", nil), (long)yearInt];
+            textFieldText = [self.loanData termValueString];
             break;
         }
         default:
