03391d4bdc9cad68b80453eb154487333f8b54c7
kimjeonghwan <dotnetguy83@gmail.com>
2014-09-01 16:52:18 +0900

    *Wallet - AddItem, iPhone, 뷰 호출 코드 수정.

--- AppBox3/Apps/Wallet/ViewController/A3WalletAllViewController.m
+++ AppBox3/Apps/Wallet/ViewController/A3WalletAllViewController.m
@@ -490,14 +490,17 @@ enum SortingKind {
 - (void)addWalletItemAction {
 	A3WalletItemEditViewController *viewController = [self itemAddViewController];
 
-//	UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:viewController];
-//	[self presentViewController:nav animated:YES completion:NULL];
-//    sdf
-    A3RootViewController_iPad *rootViewController = [[A3AppDelegate instance] rootViewController];
-    [rootViewController presentCenterViewController:[[A3NavigationController alloc] initWithRootViewController:viewController]
-                                 fromViewController:self
-                                     withCompletion:^{
-                                     }];
+    if (IS_IPAD) {
+        A3RootViewController_iPad *rootViewController = [[A3AppDelegate instance] rootViewController];
+        [rootViewController presentCenterViewController:[[A3NavigationController alloc] initWithRootViewController:viewController]
+                                     fromViewController:self
+                                         withCompletion:^{
+                                         }];
+    }
+    else {
+        UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:viewController];
+        [self presentViewController:nav animated:YES completion:NULL];
+    }
 }
 
 #pragma mark Instruction Related
