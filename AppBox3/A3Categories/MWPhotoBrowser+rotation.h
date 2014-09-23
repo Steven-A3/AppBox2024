a79fa75729d1657da3ea808b3aeb75f0660c843e
Steven <cs@allaboutapps.net>
2014-09-23 18:47:11 +0900

    Wallet 사진, 아이폰 좌우 회전 지원

---
+++ AppBox3/A3Categories/MWPhotoBrowser+rotation.h
@@ -0,0 +1,13 @@
+//
+//  MWPhotoBrowser+rotation.h
+//  AppBox3
+//
+//  Created by Byeong Kwon Kwak on 9/23/14.
+//  Copyright (c) 2014 ALLABOUTAPPS. All rights reserved.
+//
+
+#import "MWPhotoBrowser.h"
+
+@interface MWPhotoBrowser (rotation)
+
+@end
