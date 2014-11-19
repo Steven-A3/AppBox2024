76ad8022baa5e9e00676c970e00a7a12103bbc45
Steven <cs@allaboutapps.net>
2014-11-19 17:59:57 +0900

    iPad2 iOS8 약속을 클릭하고 이벤트추가 > 두개이상의 이벤트를 입력한 > 약속 리스트 오른쪽 끝 > swipe > Delete  이벤트를 삭제할 때 맨 밑에 있는 이벤트를 먼저 삭제하면 나머지 이벤트가 삭제 버튼이 나오지 않는 현상 수정

--- AppBox3/Apps/DaysCounter/2.CalendarList/A3DaysCounterEventListViewController.m
+++ AppBox3/Apps/DaysCounter/2.CalendarList/A3DaysCounterEventListViewController.m
@@ -702,7 +702,11 @@ NSString *const A3DaysCounterListSortKeyName = @"name";
 		if (item) {
 			[_sharedManager removeEvent:item inContext:item.managedObjectContext];
 		}
-		[self loadEventData];
+		double delayInSeconds = 0.5;
+		dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+		dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+			[self loadEventData];
+		});
     }
 }
 
