41d4b16ae7598a2f9a37a69690013cb172bb6164
Steven <cs@allaboutapps.net>
2015-03-25 17:57:32 +0900

    Open Source License 정보 추가

---
+++ AppBox3/MainView/About/A3OpenSourceLicensesViewController.h
@@ -0,0 +1,13 @@
+//
+//  A3OpenSourceLicensesViewController.h
+//  AppBox3
+//
+//  Created by Byeong Kwon Kwak on 3/24/15.
+//  Copyright (c) 2015 ALLABOUTAPPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface A3OpenSourceLicensesViewController : UIViewController
+
+@end
---
+++ AppBox3/MainView/About/A3OpenSourceLicensesViewController.m
@@ -0,0 +1,43 @@
+//
+//  A3OpenSourceLicensesViewController.m
+//  AppBox3
+//
+//  Created by Byeong Kwon Kwak on 3/24/15.
+//  Copyright (c) 2015 ALLABOUTAPPS. All rights reserved.
+//
+
+#import "A3OpenSourceLicensesViewController.h"
+
+@interface A3OpenSourceLicensesViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+@end
+
+@implementation A3OpenSourceLicensesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+	self.title = @"Open Source Licenses";
+	NSString *path = [[NSBundle mainBundle] pathForResource:@"openSourceLicenses" ofType:@"txt"];
+	NSString *contents = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+	self.textView.text = contents;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
