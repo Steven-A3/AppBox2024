4c0cad9ff915572cc440bb22f932cd45475dab05
Steven Kwak <cs@allaboutapps.net>
2015-10-11 22:08:37 +0900

    코드 정리

--- AppBox3/Apps/Magnifier/A3CameraViewController.h
+++ AppBox3/Apps/Magnifier/A3CameraViewController.h
@@ -6,7 +6,6 @@
 //  Copyright (c) 2014 ALLABOUTAPPS. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import <AssetsLibrary/AssetsLibrary.h>
 
 @interface A3CameraViewController : UIViewController
