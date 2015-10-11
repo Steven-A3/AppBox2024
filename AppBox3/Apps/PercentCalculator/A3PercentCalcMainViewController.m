1eceaac173916779db9b498dc5caa44ba3057262
Steven Kwak <cs@allaboutapps.net>
2015-10-11 22:16:53 +0900

    광고방식 변경

--- AppBox3/Apps/PercentCalculator/A3PercentCalcMainViewController.m
+++ AppBox3/Apps/PercentCalculator/A3PercentCalcMainViewController.m
@@ -168,12 +168,6 @@
 	if (IS_IPHONE && IS_PORTRAIT) {
 		[self leftBarButtonAppsButton];
 	}
-    if ([self isBeingPresented] || [self isMovingToParentViewController]) {
-        FNLOG(@"==============================");
-        FNLOG(@"Calling presentInterstitialAds");
-        FNLOG(@"==============================");
-        [self presentInterstitialAds];
-    }
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
