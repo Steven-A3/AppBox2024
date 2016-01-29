91916f8cbca0e74d230445415eb6cbcce423d970
Steven Kwak <cs@allaboutapps.net>
2016-01-29 18:23:47 +0900

    Hexagon 메뉴 구현 작업 진행 중

---
+++ AppBox3/MainView/Hexagon Menu/A3HexagonButton.h
@@ -0,0 +1,13 @@
+//
+//  A3HexagonButton.h
+//  AppBox3
+//
+//  Created by Byeong Kwon Kwak on 1/26/16.
+//  Copyright © 2016 ALLABOUTAPPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface A3HexagonButton : UIButton
+
+@end
