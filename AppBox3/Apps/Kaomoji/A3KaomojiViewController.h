334af68bd4de4ef9c92f55ae223e9c6932204cbf
Byeong Kwon Kwak <cs@allaboutapps.net>
2017-02-04 11:22:29 +0900

    Abbreviation 구현 중

---
+++ AppBox3/Apps/Kaomoji/A3KaomojiViewController.h
@@ -0,0 +1,13 @@
+//
+//  A3KaomojiViewController.h
+//  AppBox3
+//
+//  Created by Byeong-Kwon Kwak on 2/4/17.
+//  Copyright © 2017 ALLABOUTAPPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface A3KaomojiViewController : UIViewController
+
+@end
---
+++ AppBox3/Apps/Kaomoji/A3KaomojiViewController.m
@@ -0,0 +1,37 @@
+//
+//  A3KaomojiViewController.m
+//  AppBox3
+//
+//  Created by Byeong-Kwon Kwak on 2/4/17.
+//  Copyright © 2017 ALLABOUTAPPS. All rights reserved.
+//
+
+#import "A3KaomojiViewController.h"
+
+@interface A3KaomojiViewController ()
+
+@end
+
+@implementation A3KaomojiViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
